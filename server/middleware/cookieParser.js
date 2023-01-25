c887e58c5482574d485fb7c9986c51b07e06ba30
kahlstorf1 <dkahlstorf1@gmail.com>
2023-01-24 23:27:45 -0500

    Commit progress through Tues

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,23 @@
 const parseCookies = (req, res, next) => {
+  console.log('cooks', req.headers.cookie);
+  var sessionCookies = req.headers.cookie;
+
+  if (!sessionCookies) {
+    return {};
+  }
+
+//fix object storage / extra spaces
+
+  var splitCookies = sessionCookies.split(';');
+  console.log(splitCookies);
+  for (var i = 0; i < splitCookies.length; i++) {
+    for (var j = 0; j < splitCookies[i].length; j++) {
+      if (splitCookies[i][j] === '=') {
+        req.cookies[splitCookies[i].slice(0, j)] = splitCookies[i].slice(j, splitCookies[i].length - 1);
+      }
+    }
+  }
+  console.log('rc', req.cookies)
 };
 
 module.exports = parseCookies;
